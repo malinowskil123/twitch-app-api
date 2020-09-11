7bbbc7659bf1e86f5ef6d04db11ea6932f389686
Lukasz Malinowski <lukaszmalinowski@MacBook-Pro.local>
2020-09-10 22:44:52 -0400

    bug fix

--- server/utils/cache.js
+++ server/utils/cache.js
@@ -1,6 +1,6 @@
-// redis set up
-const redis = require('redis')
-const REDIS_PORT = process.env.REDIS_PORT || 6379
-const client = redis.createClient(REDIS_PORT)
+// // redis set up
+// const redis = require('redis')
+// const REDIS_PORT = process.env.REDIS_PORT || 6379
+// const client = redis.createClient(REDIS_PORT)
 
-module.exports = client
+// module.exports = client
