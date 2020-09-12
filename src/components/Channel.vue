fcbb34e79674c1f867b52a432d79e1507b3d21e7
Lukasz Malinowski <lukaszmalinowski@MacBook-Pro.local>
2020-09-12 15:48:25 -0400

    channel img resized

--- src/components/Channel.vue
+++ src/components/Channel.vue
@@ -13,54 +13,58 @@
         Followers:
         <strong>{{ channel.followers }}</strong>
       </p>
-      <button @click="$emit('delete-channel', channel._id)" class="del">
-        X
-      </button>
+      <button @click="$emit('delete-channel', channel._id)" class="del">X</button>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'Channel',
-    props: ['channel'],
-  }
+export default {
+  name: "Channel",
+  props: ["channel"],
+};
 </script>
 
 <style scoped>
-  .channel {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-content: center;
-    color: #333;
-    background: #f3f3f3;
-    margin: 20px 0px;
-    border-radius: 10px;
-  }
+.channel {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  color: #333;
+  background: #f3f3f3;
+  margin: 20px 0px;
+  border-radius: 10px;
+}
 
-  .channel:hover {
-    background: #c7c6c6;
-    transition: all 0.3s ease;
-  }
+.channel:hover {
+  background: #c7c6c6;
+  transition: all 0.3s ease;
+}
 
+.channel-img {
+  width: 15%;
+  border-radius: 50%;
+}
+
+@media (min-width: 0px) and (max-width: 576px) {
   .channel-img {
-    width: 10%;
-    border-radius: 50%;
+    width: 25%;
   }
+}
 
-  .del {
-    background: #ff0000;
-    color: #fff;
-    margin-left: 10px;
-    border: none;
-    padding: 5px 9px;
-    border-radius: 50%;
-    cursor: pointer;
-    float: right;
-  }
+.del {
+  background: #ff0000;
+  color: #fff;
+  margin-left: 10px;
+  border: none;
+  padding: 5px 9px;
+  border-radius: 50%;
+  cursor: pointer;
+  float: right;
+}
 
-  .del:hover {
-    background: #b81111;
-  }
+.del:hover {
+  background: #b81111;
+}
 </style>
